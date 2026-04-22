b78765d06218794c721c52f9eb89781a08299997
2007bharat <bandaupretibisht@gmail.com>
2026-04-22 15:50:17 +0530

    bharat

---
+++ react-starter/src/App.jsx
@@ -0,0 +1,20 @@
+import Card from "./component/card"
+function APP(){
+
+  return (
+    <>
+      <div id="dad">
+            <h1>Hello</h1>
+      </div>
+      <div className="card">
+
+        <Card user="bhart" />
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates rerum animi repellendus eum numquam temporibus odit earum natus nihil? Explicabo sunt molestias iste, voluptas quo laudantium labore debitis quidem, nulla earum vitae qui animi amet eos dolores enim accusamus fugiat voluptatibus atque. Eaque, atque earum?</div>
+      </div>
+     
+    </>
+  )
+}
+
+
+export default APP
---
+++ react-starter/src/component/card.jsx
@@ -0,0 +1,11 @@
+function Card(props){
+    console.log(props)
+    return(
+       
+        console.log(props.user)
+       
+    )
+}
+
+
+export default Card;
