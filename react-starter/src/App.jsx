7434fdcdac9e2494789b77da54645e1bf3823b0b
2007bharat <bandaupretibisht@gmail.com>
2026-04-22 17:31:38 +0530

    third commit

--- react-starter/src/App.jsx
+++ react-starter/src/App.jsx
@@ -1,20 +1,13 @@
 import Card from "./component/card"
-function APP(){
 
-  return (
-    <>
-      <div id="dad">
-            <h1>Hello</h1>
-      </div>
-      <div className="card">
+function App(){
 
-        <Card user="bhart" />
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates rerum animi repellendus eum numquam temporibus odit earum natus nihil? Explicabo sunt molestias iste, voluptas quo laudantium labore debitis quidem, nulla earum vitae qui animi amet eos dolores enim accusamus fugiat voluptatibus atque. Eaque, atque earum?</div>
-      </div>
-     
-    </>
-  )
+ return (
+  <div className="parent">
+      <Card />
+  </div>
+ )
 }
 
 
-export default APP
+export default App;
--- react-starter/src/component/card.jsx
+++ react-starter/src/component/card.jsx
@@ -1,13 +1,20 @@
-function Card(props){
-    console.log(props)
-    return(
-       
-
-            <div>
-                
-            </div>
-       
-    )
+import {User} from "lucide-react"
+function Card(){
+    return (
+        <>
+    <div className="top">
+        <User size={16} strokeWidth={1.5} />
+        <h2>Welcome Back</h2>
+        <p>Please enter your info to sing in to zaap</p>
+        <div className="top-child">
+            <img src="https://media.wired.com/photos/5926ffe47034dc5f91bed4e8/3:2/w_2560%2Cc_limit/google-logo.jpg" alt="Google-Image" />
+            <img src="https://img.freepik.com/premium-psd/facebook-logo-blue-circle_705838-12823.jpg?semt=ais_hybrid&w=740&q=80" alt="Facebok-Image" />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH9EcrbYSmK47DQTwAvrqRS9Hoyjuyyxi8gA&s" alt="apple-image" />
+        </div>
+    </div>
+    
+        </>
+)
 }
 
 
